8747f928ac6b9371bb058a2164a5af1fd922df65
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-02-19 15:19:58 -0300

    add functionality to append images by link

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
 
-    //selector in jquery
-    $('header button')
+    $('header button').click(function() {
+        $('form').slideDown();
+    });
 
+    $('#btn-cancel').click(function() {
+        $('form').slideUp();
+    })
+
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const enderecoNovaImagem = $('#url-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+
+
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">Ver imagem em tamanho real</a>  
+            </div>
+        `).appendTo(novoItem);
+
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(250);
+
+        $('#url-imagem-nova').val('');
+    });
 })
